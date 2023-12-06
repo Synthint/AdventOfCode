a1500ebdf1dcdfdd9ca1fc4ab24b12134d60a39a
Sean <seanm0914@gmail.com>
2023-12-06 11:40:08 -0500

    Day 6 challenge and fix testing and running for python solves

--- 2023/day_04/test_main.py
+++ 2023/day_04/test_main.py
@@ -19,15 +19,3 @@ def test_solve_part_1_example(input,actual):
                           [("test_input.txt", PART_2_EXAMPLE_SOLUTION)])
 def test_solve_part_2_example(input,actual):
     assert solve_part_2(input) == actual
-
-# Test the solution for part 1 works for the actual input
-@pytest.mark.parametrize("input, actual",
-                          [("input.txt", PART_1_ACTUAL_SOLUTION)])
-def test_solve_part_1_actual(input,actual):
-    assert solve_part_1(input) == actual
-
-# Test the solution for part 2 works for the actual input
-@pytest.mark.parametrize("input, actual",
-                          [("input.txt", PART_2_ACTUAL_SOLUTION)])
-def test_solve_part_2_actual(input,actual):
-    assert solve_part_2(input) == actual
