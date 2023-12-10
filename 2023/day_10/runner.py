706b657ba6d2964ea05e02a598e0d47f71b2882d
Sean Meyer <30555872+Synthint@users.noreply.github.com>
2023-12-10 13:55:43 +0000

    day 10 soultion

--- 2023/day_10/runner.py
+++ 2023/day_10/runner.py
@@ -1,5 +1,5 @@
 from main import solve_part_1, solve_part_2
 
 
-print("Part 1 Answer ->",solve_part_1("./input.txt"))
+#print("Part 1 Answer ->",solve_part_1("./input.txt"))
 print("Part 2 Answer ->",solve_part_2("./input.txt"))
--- 2023/day_10/test_main.py
+++ 2023/day_10/test_main.py
@@ -1,11 +1,8 @@
 from main import solve_part_1, solve_part_2
 import pytest
 
-PART_1_EXAMPLE_SOLUTION = None
-PART_2_EXAMPLE_SOLUTION = None
-
-PART_1_ACTUAL_SOLUTION = None
-PART_2_ACTUAL_SOLUTION = None
+PART_1_EXAMPLE_SOLUTION = 8
+PART_2_EXAMPLE_SOLUTION = 8
 
 
   # Test the solution for part 1 works for the example input
@@ -16,18 +13,6 @@ def test_solve_part_1_example(input,actual):
 
 # Test the solution for part 2 works for the example input
 @pytest.mark.parametrize("input, actual",
-                          [("test_input.txt", PART_2_EXAMPLE_SOLUTION)])
+                          [("test_input_2.txt", PART_2_EXAMPLE_SOLUTION)])
 def test_solve_part_2_example(input,actual):
-    assert solve_part_2(input) == actual
-
-# Test the solution for part 1 works for the actual input
-@pytest.mark.parametrize("input, actual",
-                          [("input.txt", PART_1_ACTUAL_SOLUTION)])
-def test_solve_part_1_actual(input,actual):
-    assert solve_part_1(input) == actual
-
-# Test the solution for part 2 works for the actual input
-@pytest.mark.parametrize("input, actual",
-                          [("input.txt", PART_2_ACTUAL_SOLUTION)])
-def test_solve_part_2_actual(input,actual):
     assert solve_part_2(input) == actual
