20f97a9648a13c5f820ce1c9917fca6e0333ab08
Sean Meyer <30555872+Synthint@users.noreply.github.com>
2023-12-10 02:38:38 +0000

    gen files for solution

---
+++ 2023/day_10/main.py
@@ -0,0 +1,9 @@
+import re
+
+def solve_part_1(file):
+    with open(file, "r") as input:
+        return None
+        
+def solve_part_2(file):
+    with open(file, "r") as input:
+        return None
