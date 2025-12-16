6e44dbdd1ca84c9c8bd5069d9862f9c37cdaeca1
Sean Meyer <30555872+Synthint@users.noreply.github.com>
2025-12-16 03:31:04 +0000

    refactor for shared solution

--- AoC2025/Day_03/solution.go
+++ AoC2025/Day_03/solution.go
@@ -8,47 +8,11 @@ import (
 )
 
 func SolvePartOne(file_slice []string) string {
-	grand_total := 0
+	return solveForNumDigits(file_slice, 2)
+}
 
-	for _, bank := range file_slice {
-		bank_total := 0
-		max := 0
-		for _, jolt := range strings.Split(bank, "") {
-			if utils.Atoi(jolt) > max {
-				max = utils.Atoi(jolt)
-			}
-		}
-
-		bank_split := strings.Split(bank, fmt.Sprintf("%d", max))
-		if len(bank_split) == 2 {
-			mini_bank := bank_split[1]
-			max_second_digit := false
-			if len(bank_split[1]) == 0 {
-				max_second_digit = true
-				mini_bank = bank_split[0]
-			}
-
-			max_2 := 0
-			for _, jolt := range strings.Split(mini_bank, "") {
-				if utils.Atoi(jolt) > max_2 {
-					max_2 = utils.Atoi(jolt)
-				}
-			}
-
-			if max_second_digit {
-				bank_total = utils.Atoi(fmt.Sprintf("%d%d", max_2, max))
-			} else {
-				bank_total = utils.Atoi(fmt.Sprintf("%d%d", max, max_2))
-			}
-		} else {
-			bank_total = utils.Atoi(fmt.Sprintf("%d%d", max, max))
-		}
-
-		// fmt.Printf("Bank %s had a joltage of %d \n", bank, bank_total)
-		grand_total += bank_total
-	}
-
-	return fmt.Sprintf("%d", grand_total)
+func SolvePartTwo(file_slice []string) string {
+	return solveForNumDigits(file_slice, 12)
 }
 
 func getLargestDigitBeforeIndex(bank string, index int) (int, int, error) {
@@ -70,9 +34,8 @@ func getLargestDigitBeforeIndex(bank string, index int) (int, int, error) {
 	return max_index, max, nil
 }
 
-func SolvePartTwo(file_slice []string) string {
+func solveForNumDigits(file_slice []string, num_digits_wanted int) string {
 	grand_total := 0
-	num_digits_wanted := 12
 	for _, bank := range file_slice {
 		cutoff_index := num_digits_wanted - 1
 		bank_total_str := ""
