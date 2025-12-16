05acdf2195b648edbfc4cc538493f525d703422d
Sean Meyer <30555872+Synthint@users.noreply.github.com>
2025-12-16 00:52:52 +0000

    Implemented day 1 part 2

--- AoC2025/Day_01/solution.go
+++ AoC2025/Day_01/solution.go
@@ -3,7 +3,6 @@ package template
 import (
 	utils "AoC2025/utils"
 	"fmt"
-	"math"
 )
 
 func SolvePartOne(file_slice []string) string {
@@ -11,7 +10,6 @@ func SolvePartOne(file_slice []string) string {
 	pass_0 := 0
 
 	current_position := start
-	fmt.Printf("aa")
 	for _, line := range file_slice {
 		clicks := line[1:]
 		clicks_int := utils.Atoi(clicks)
@@ -24,7 +22,6 @@ func SolvePartOne(file_slice []string) string {
 			pass_0++
 		}
 	}
-
 	return fmt.Sprintf("%d", pass_0)
 }
 
@@ -33,21 +30,42 @@ func SolvePartTwo(file_slice []string) string {
 	pass_0 := 0
 
 	current_position := start
-	fmt.Printf("aa")
 	for _, line := range file_slice {
 		clicks := line[1:]
 		clicks_int := utils.Atoi(clicks)
+
+		if clicks_int > 100 {
+			pass_0 += clicks_int / 100
+			clicks_int = clicks_int % 100
+		}
+
 		if line[:1] == "L" {
 			clicks_int *= -1
 		}
 		old_position := current_position
 		current_position += clicks_int
-		if current_position == 0 || current_position%100 == 0 || math.Abs(float64(current_position-old_position)) > 100 {
-			current_position = 0
+		passed_0 := false
+		if current_position == 0 {
 			pass_0++
+			passed_0 = true
+		} else if current_position/100 > 0 {
+			pass_0 += current_position / 100
+			passed_0 = true
+		}
+
+		if current_position < 0 {
+			current_position += 100
+		}
+		current_position = current_position % 100
+
+		if line[:1] == "L" && old_position < current_position && old_position != 0 && !passed_0 {
+			pass_0++
+			passed_0 = true
+		} else if line[:1] == "R" && old_position > current_position && old_position != 0 && !passed_0 {
+			pass_0++
+			passed_0 = true
 		}
 	}
-
 	return fmt.Sprintf("%d", pass_0)
 }
 
